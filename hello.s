5f1e659bf36c4e9901163404378ca453501f6a34
mathieu <mathieu.hautebas@gmail.com>
2017-09-04 00:08:57 +0200

    I hate spaces

--- hello.s
+++ hello.s
@@ -1,5 +1,5 @@
 section .data
-	text db "Hello World", 10 ; 10 = '\n'
+	text db "we got it", 10 ; 10 = '\n'
 	
 section .text
 	global _start 
@@ -14,5 +14,5 @@ _start:
 
 .exit:
 	mov 	rax, 60 ; 	Number of exit syscall
-	mov		rdi, 0
+	mov 	rdi, 0
 	syscall
